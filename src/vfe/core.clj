b93fec324bf7cd11e48b2beeedd854fdfac99fb3
Robin Walsh <robin@counsyl.com>
2016-04-14 11:57:02 -0700

    slowly, not surely

--- src/vfe/core.clj
+++ src/vfe/core.clj
@@ -17,23 +17,55 @@
 (def users {"root" {:username "root"
                     :password (creds/hash-bcrypt "admin_password")
                     :roles #{::admin}}
+            "robin" {:username "robin"
+                     :password (creds/hash-bcrypt "asd")
+                     :roles #{::user ::admin}}
             "jane" {:username "jane"
                     :password (creds/hash-bcrypt "user_password")
                     :roles #{::user}}})
 
 
 (defn index [request]
+  (prn request)
   (html
-    [:h1 "Hebbo world"]))
+    [:h1 "Hebbo world"]
+    ))
+
+
+
+(defn login [request]
+  (html
+    [:h1 "Hokay. Log in plox."
+    [:div
+     [:form {:method "POST" :action "/login"}
+      [:p
+       [:input {:type "text" :name "username" :placeholder "username"}]]
+      [:p
+       [:input {:type "password" :name "password" :placeholder "assword"}]]
+      [:p 
+       [:input {:type "submit" :name "commit" :value "Login"}]]
+
+       
+      ]
+     ]]
+    )
+  )
+
+(defn wrap-prn [handler]
+  (fn [request]
+    (prn request)
+    (handler request)))
 
 (defroutes unsecured-app
   (GET "/"  [request] (index request))
+  (GET "/login"  [request] (login request))
   (route/not-found "<h1>Page not found</h1>"))
 
 (def app
   (-> unsecured-app
     (friend/authenticate {:credential-fn (partial creds/bcrypt-credential-fn users)
                           :workflows [(workflows/interactive-form)]})
+    (wrap-prn)
     (wrap-params)
     (wrap-nested-params)
     (wrap-keyword-params)
